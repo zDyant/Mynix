24f684c5c07fab2b6867e25e70f08c373e4379c0
zDyantB <zdyant@protonmail.com>
2024-03-29 11:08:17 -0300

    Fix commands on startup.nix script

--- modules/home/hypr/configs/startup.nix
+++ modules/home/hypr/configs/startup.nix
@@ -2,9 +2,6 @@
 {
 wayland.windowManager.hyprland = {
   extraConfig = "
-$scriptsDir = $HOME/.config/hypr/scripts
-$UserScripts = $HOME/.config/hypr/UserScripts
-
 $wallDIR=$HOME/Pictures/wallpapers
 
 # wallpaper stuff / More wallpaper options below
@@ -15,25 +12,22 @@ exec-once = dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CUR
 exec-once = systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
 # Polkit (Polkit Gnome / KDE)
-exec-once = $scriptsDir/Polkit.sh
+#exec-once = polkit
 
 # starup apps
 exec-once = waybar &
 exec-once = nm-applet --indicator &
 exec-once = swaync &
-#exec-once = blueman-applet & 
-#exec-once = rog-control-center &
-
 
 #clipboard manager
 exec-once = wl-paste --type text --watch cliphist store 
 exec-once = wl-paste --type image --watch cliphist store
 
-#gnome polkit for nixos
-#exec-once = $scriptsDir/Polkit-NixOS.sh
+# Gnome polkit for nixos
+exec-once = poklit-nixos
 
 # xdg-desktop-portal-hyprland (should be auto starting. However, you can force to start)
-#exec-once = $scriptsDir/PortalHyprland.sh
+#exec-once = portal-hyprland
 
     ";
   };
