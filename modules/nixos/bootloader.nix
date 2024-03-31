1c0170c1bfd3e0f12c59b4bde9c36cbab5a87c04
zDyantB <zdyant@protonmail.com>
2024-03-31 19:18:07 -0300

    Add plymouth config

---
+++ modules/nixos/bootloader.nix
@@ -0,0 +1,33 @@
+{ pkgs, ... }:
+{
+  # boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.systemd.enable = true;
+  # Don't show command line arguments on boot
+  boot.kernelParams = [ "quiet" ];
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "/dev/nvme0n1";
+
+    theme = pkgs.stdenv.mkDerivation {
+      pname = "catppuccin-grub";
+      version = "1.0";
+      src = pkgs.fetchFromGitHub
+        {
+          owner = "catppuccin";
+          repo = "grub";
+          rev = "88f6124";
+          sha256 = "sha256-e8XFWebd/GyX44WQI06Cx6sOduCZc5z7/YhweVQGMGY=";
+        };
+      installPhase = "cp -r src/catppuccin-mocha-grub-theme $out";
+    };
+  };
+
+  boot.plymouth = {
+    enable = true;
+    themePackages = [ pkgs.catppuccin-plymouth ];
+    theme = "catppuccin-macchiato";
+  };
+
+}
