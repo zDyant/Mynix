0a930c0ad0c8e589b9acf93898e669efb0e03572
zDyantB <zdyant@protonmail.com>
2024-04-05 19:09:55 -0300

    Add wlogout config

---
+++ modules/home/wlogout/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    wlogout
+  ];
+
+  xdg.configFile."wlogout" = {
+    source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/modules/home/wlogout;
+    recursive = true;
+  };
+}
