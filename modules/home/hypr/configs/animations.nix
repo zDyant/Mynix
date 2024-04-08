02715fb8711909f424eb35d839f358df492daa3e
zDyantB <zdyant@protonmail.com>
2024-04-08 18:16:21 -0300

    Update: swipe animation

--- modules/home/hypr/configs/animations.nix
+++ modules/home/hypr/configs/animations.nix
@@ -7,7 +7,7 @@
   enabled = yes
 
   bezier = shot, 0.2, 1.0, 0.2, 1.0
-  bezier = swipe, 0.8, 0.0, 0.3, 1.05
+  bezier = swipe, 0.6, 0.0, 0.2, 1.05
   bezier = linear, 0.0, 0.0, 1.0, 1.0
   bezier = progressive, 1.0, 0.0, 0.6, 1.0
 
