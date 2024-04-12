2cf32465acda90c0c99fd4b35fc96b2072972ab3
zDyantB <zdyant@protonmail.com>
2024-04-12 15:34:35 -0300

    Update: packages

--- modules/home/games/default.nix
+++ modules/home/games/default.nix
@@ -2,14 +2,21 @@
 {
 
   home.packages = with pkgs; [
+    protontricks
+    protonup-qt
     bottles
     heroic
     gamescope
     adwsteamgtk
-    wineWowPackages.staging
+    wineWowPackages.waylandFull
     winetricks
     mesa
     mangohud
+
+    # emulators
+    pcsx2
+    # rpcs3
+    ryujinx
   ];
   
 }
