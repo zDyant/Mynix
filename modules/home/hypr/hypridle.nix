255c1962ad1f1c2f28a981d348b03cceea5e2b0f
zDyantB <zdyant@protonmail.com>
2024-04-14 22:08:00 -0300

    Update: hypridle

--- modules/home/hypr/hypridle.nix
+++ modules/home/hypr/hypridle.nix
@@ -6,9 +6,10 @@ home.file.".config/hypr/hypridle.conf".text = ''
 general {
     # lock_cmd = notify-send "lock!"          # dbus/sysd lock command (loginctl lock-session) 
     # unlock_cmd = notify-send "unlock!"      # same as above, but unlock
-    # before_sleep_cmd = notify-send "Zzz"    # command ran before sleep
-    # after_sleep_cmd = notify-send "Awake!"  # command ran after sleep
     ignore_dbus_inhibit = false             # whether to ignore dbus-sent idle-inhibit requests (used by e.g. firefox or steam)
+    lock_cmd = pidof hyprlock || hyprlock # avoid starting multiple hyprlock instances.
+    before_sleep_cmd = loginctl lock-session    # lock before suspend.
+    after_sleep_cmd = hyprctl dispatch dpms on  # to avoid having to press a key twice to turn on the display.
 }
 
 # Screenlock
