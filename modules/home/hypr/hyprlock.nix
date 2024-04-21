62e3158f8cbe1faaf563374c6b86248e2beebcc4
zDyantB <zdyant@protonmail.com>
2024-04-21 11:42:54 -0300

    Update: hyprlock theme

--- modules/home/hypr/hyprlock.nix
+++ modules/home/hypr/hyprlock.nix
@@ -7,7 +7,7 @@ source = $HOME/.cache/wal/colors-hyprland.conf
 background {
     monitor =
     path = $HOME/.current_wallpaper   # only png supported for now
-    color = $color1
+    # color = $color1
 
     # all these options are taken from hyprland, see https://wiki.hyprland.org/Configuring/Variables/#blur for explanations
     blur_size = 4
@@ -23,12 +23,12 @@ input-field {
     monitor =
     size = 250, 50
     outline_thickness = 3
-    dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+    dots_size = 0.26 # Scale of input-field height, 0.2 - 0.8
     dots_spacing = 0.64 # Scale of dots' absolute size, 0.0 - 1.0
     dots_center = true
-    outer_color = $color1
+    outer_color = $color0
     inner_color = $color0
-    font_color = $color7
+    font_color = $color6
     fade_on_empty = true
     placeholder_text = <i>Password...</i> # Text rendered in the input box when it's empty.
     hide_input = false
@@ -42,9 +42,11 @@ input-field {
 label {
     monitor =
     text = cmd[update:1000] echo "<b><big> $(date +"%H:%M:%S") </big></b>"
-    color = $color1
+    color = $color6
     font_size = 64
     font_family = JetBrains Mono Nerd Font 10
+    shadow_passes = 3
+    shadow_size = 4
 
     position = 0, 16
     halign = center
