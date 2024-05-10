bbdba0145ec19849e635a688d1464f6610394904
zDyant <dyant@proton.me>
2024-05-10 14:16:34 -0300

    Update: remove lvim files

--- modules/home/lvim/default.nix
+++ modules/home/lvim/default.nix
@@ -16,9 +16,9 @@
     yarn
   ];
 
-  xdg.configFile."lvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink /etc/nixos/modules/home/lvim/lvim;
-    recursive = true;
-  };
+  # xdg.configFile."lvim" = {
+  #   source = config.lib.file.mkOutOfStoreSymlink ./lvim;
+  #   recursive = true;
+  # };
 
 }
