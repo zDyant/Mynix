922933f4c778e3e25e87f7bb551b69dc7eeeb32f
zDyant <dyant@proton.me>
2024-06-10 17:03:36 -0300

    update hyprland config

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -9,9 +9,10 @@
     enable = true;
     wlr.enable = true;
     xdgOpenUsePortal = true;
-    extraPortals = [
-      # pkgs.xdg-desktop-portal-hyprland
-      # pkgs.xdg-desktop-portal-gtk
+    extraPortals = with pkgs; [
+      # xdg-desktop-portal-hyprland
+      # xdg-desktop-portal-gtk
+    #   xdg-desktop-portal-wlr
     ];
   };
 }
