15a555fa03bd4f31a60ee6dbc227625813c00e7f
zDyant <dyant@proton.me>
2024-06-10 17:06:37 -0300

    update gtk config

--- modules/home/gtk/default.nix
+++ modules/home/gtk/default.nix
@@ -1,14 +1,5 @@
 { config, pkgs, ... }:
 {
-  fonts.fontconfig.enable = true;
-  home.packages = with pkgs; [
-    (nerdfonts.override {
-      fonts = [
-      "JetBrainsMono"
-      ];
-    })
-  ];
-
   gtk = {
     enable = true;
 
