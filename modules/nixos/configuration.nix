56299eb58a559f389a65633d562b1149336d4311
zDyant <dyant@proton.me>
2024-06-11 18:46:15 -0300

    remove gnome

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -43,9 +43,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
