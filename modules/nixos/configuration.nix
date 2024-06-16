4c6c99fc6911f16a8852bb74ece2227442349479
zDyant <dyant@proton.me>
2024-06-16 07:55:06 -0300

    update xkb config

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -40,9 +40,9 @@
   services.xserver.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "us";
-    xkbVariant = "";
+    variant = "";
   };
 
   services.gvfs.enable = true;
