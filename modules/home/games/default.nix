cca9eecf076ef0b2f474e1449a07522513ebeec4
zDyant <dyant@proton.me>
2024-06-17 17:30:04 -0300

    update gaming packages

--- modules/home/games/default.nix
+++ modules/home/games/default.nix
@@ -5,6 +5,8 @@
     protonup-qt
     heroic
     adwsteamgtk # Theme for steam
+    steam-rom-manager # Emulator on steam
+    steamtinkerlaunch 
     wineWowPackages.stagingFull # Don't change!!!, wayland version is broken
     mesa
     mangohud
@@ -12,10 +14,14 @@
     sunshine
     lutris
     moonlight-qt
+    prismlauncher
+    jdk21
     # nexusmods-app
 
     # emulators -----------------------
+    # emulationstation-de
     pcsx2   # PS2
+    melonDS # NDS
     # rpcs3   # PS3
     cemu    # Wii u
     ryujinx # Switch
