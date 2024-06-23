80ae7b664b2c3d2a35bfa4d201da5501f4398e5d
zDyant <dyant@proton.me>
2024-06-23 10:39:35 -0300

    remove and change display manager

--- modules/nixos/login-screen.nix
+++ modules/nixos/login-screen.nix
@@ -1,26 +1,21 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
-
-  environment.systemPackages = with pkgs; [
-    libsForQt5.qt5.qtquickcontrols2
-    libsForQt5.qt5.qtgraphicaleffects
-    (callPackage ./sddm-rose-pine.nix {})
-  ];
-
-  
-  services.xserver = {
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+ 
+ services.xserver.displayManager.gdm.enable = true;
+  services = {
     libinput.enable = true;
     displayManager = {
+
       defaultSession = "hyprland";
       autoLogin = {
         enable = true;
         user = "zdyant";
       };
-      sddm = {
-        enable = true;
-        theme = "rose-pine";
-      };
+
     };
   };
 
