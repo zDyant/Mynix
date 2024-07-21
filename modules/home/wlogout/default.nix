b201afe895233c59f7c3ebdbca78422af0a9a203
zDyant <dyant@proton.me>
2024-07-21 11:38:22 -0300

    update wlogout theme

--- modules/home/wlogout/default.nix
+++ modules/home/wlogout/default.nix
@@ -1,11 +1,45 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 {
-  home.packages = with pkgs; [
-    wlogout
-  ];
+  programs.wlogout = {
+    enable = true;
+    style = "./nova.css";
+    layout = [
+      {
+        "label" = "shutdown";
+        "action" = "systemctl poweroff";
+        "text" = "Shutdown";
+        "keybind" = "s";
+      }
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "Reboot";
+        "keybind" = "r";
+      }
+      {
+        "label" = "logout";
+        "action" = "loginctl kill-session $XDG_SESSION_ID";
+        "text" = "Logout";
+        "keybind" = "e";
+      }
+      {
+        "label" = "hibernate";
+        "action" = "swaylock -f && systemctl hibernate";
+        "text" = "Hibernate";
+        "keybind" = "h";
+      }
+      {
+        "label" = "lock";
+        "action" = "hyprlock";
+        "text" = "Lock";
+        "keybind" = "l";
+      }
 
-  xdg.configFile."wlogout" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./wlogout;
+    ];
+  };
+
+  xdg.configFile."wlogout/icons" = {
+    source = config.lib.file.mkOutOfStoreSymlink ./icons;
     recursive = true;
   };
 }
