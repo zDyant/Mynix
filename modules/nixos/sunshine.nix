16222a5f430a8bd3725d072764f1873046e38021
zdyant <dyant@proton.me>
2024-10-20 16:58:31 -0300

    update sunshine

--- modules/nixos/sunshine.nix
+++ modules/nixos/sunshine.nix
@@ -7,11 +7,11 @@
     openFirewall = true;
   };
 
-  security.wrappers.sunshine = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_sys_admin+p";
-    source = "${pkgs.sunshine}/bin/sunshine";
-  };
+  # security.wrappers.sunshine = {
+  #   owner = "root";
+  #   group = "root";
+  #   capabilities = "cap_sys_admin+p";
+  #   source = "${pkgs.sunshine}/bin/sunshine";
+  # };
 
 }
