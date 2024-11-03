534b3c3c1b02edb4b104c2b8a581a9f5c5f0f355
zdyant <dyant@proton.me>
2024-11-03 10:56:03 -0300

    update kitty config

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -1,22 +1,25 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.kitty = {
     enable = true;
     theme = "Catppuccin-Mocha";
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 11;
-    };
+    # font = {
+    #   name = "JetBrainsMono Nerd Font";
+    #   size = 11;
+    # };
     settings = {
       mouse_hide_wait = 0;
       confirm_os_window_close = 0;
       enable_audio_bell = false;
+      background_opacity = "0.5";
+      background_blur = 5;
 
       # tab_bar_min_tabs            1
       tab_bar_edge = "bottom";
       tab_bar_style = "powerline";
       tab_powerline_style = "round";
       tab_title_template = "{title}{' :{}:'.format(num_windows) if num_windows > 1 else ''}";
+
     };
 
   };
