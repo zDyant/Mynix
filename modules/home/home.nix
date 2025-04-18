4fefdb08918538d6774cf9d54a88aff1d9c44e9a
zDyant <dyant@proton.me>
2025-04-18 07:12:22 -0300

    remove obsolete variables on home.nix

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -38,14 +38,6 @@
 	GTK_USE_PORTAL = "1";
     NIXOS_OZONE_WL = "1";
 	NIXOS_XDG_OPEN_USE_PORTAL = "1";
-
-	XDG_CACHE_HOME = "$HOME/.cache";
-	XDG_CONFIG_HOME = "$HOME/.config";
-	XDG_BIN_HOME = "$HOME/.local/bin";
-	XDG_DATA_HOME = "$HOME/.local/share";
-
-    # set bat theme for good
-    BAT_THEME="matugen";
   };
 
   # Add to $PATH
