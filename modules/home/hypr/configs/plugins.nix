b4c91b5dcf07f00ec5416b12e3204be1348a8e6a
zDyant <dyant@proton.me>
2025-07-16 10:15:06 -0300

    update hypr plugins config

--- modules/home/hypr/configs/plugins.nix
+++ modules/home/hypr/configs/plugins.nix
@@ -20,14 +20,20 @@
       # quadratic          - a quadratic function is used
       # negative_quadratic - negative version of the quadratic one, feels more aggressive
       # see `activation` in `src/mode/utils.cpp` for how exactly the calculation is done
-      function = quadratic
+      function = negative_quadratic
 
       # time window (ms) over which the speed is calculated
       # higher values will make slow motions smoother but more delayed
-      window = 200
+      window = 100
     }
     # shaperule = clientside, none
-  }
+    
+    shake = {
+        # controls how soon a shake is detected
+        # lower values mean sooner
+        threshold = 4
+    }
+  
 
   ";
   };
