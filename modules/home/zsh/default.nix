c3725b2dbffebc6be8c7a0bf559173a6721496cb
zDyant <dyant@proton.me>
2025-08-18 12:23:52 -0300

    update zsh aliases

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -22,18 +22,8 @@
       cdi = "zi";
       ls = "exa --icons";
 
-      ga = "git add";
-      gst = "git status";
-      gf = "git diff | diff-so-fancy";
-      gfs = "git diff --staged";
-      gm = "git commit -m";
-      gr = "git restore";
-      grs = "git restore --staged";
       gpull = "git pull --ff-only";
 
-      # fi = "fzf --height 60% --preview='kitten icat --clear --transfer-mode=memory --stdin=no --place=\${FZF_PREVIEW_COLUMNS}x\${FZF_PREVIEW_LINES}@0x0 {}' && sleep 0.5 ; printf 'x1b_Ga=d,d=Ax1b\\\\' ";
-      # ff = "fzf -q '!png !jpg !jpeg !gif !bmp' --preview 'bat --color=always {}'";
-      # bevy = "nix-shell --run \"cargo run\"";
 
       pkg = "nix-shell -p";
       upkg = "nix-shell -I nixpkgs=channel:nixpkgs-unstable -p";
