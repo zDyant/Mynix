c7dc067b1365779107956d2a72612092831e4bdc
zDyant <dyant@proton.me>
2025-09-07 19:58:40 -0300

    update: configuration.nix

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -113,18 +113,42 @@
     options = "--delete-older-than 7d";
   };
 
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
-
   # Making sure /tmp is clear to avoid issues
   boot.tmp.cleanOnBoot = true;
+  boot.tmp.useTmpfs = true;
+
+  # WE DONT WANT TO BUILD STUFF ON TMPFS
+  # ITS NOT A GOOD IDEA
+  systemd.services.nix-daemon = {
+    environment.TMPDIR = "/var/tmp";
+  };
 
   # WARN Don't change -------------------------------------
   system.stateVersion = "23.11"; # Did you read the comment?
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+
+  nix = {
+    settings = {
+      # STFU
+      warn-dirty = false;
+      # https://bmcgee.ie/posts/2023/12/til-how-to-optimise-substitutions-in-nix/
+      http-connections = 128;
+      max-substitution-jobs = 128;
+      # use binary cache, its not gentoo
+      builders-use-substitutes = true;
+      auto-optimise-store = true;
+      max-jobs = "auto";
+
+      # continue building derivations if one fails
+      keep-going = true;
+
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+    };
+
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   # Some shenanigans --------------------------------------
