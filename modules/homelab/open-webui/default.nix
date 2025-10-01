549fd2c9e7e610a8c01d5bf9a0f69aa366ef527f
zDyant <dyant@proton.me>
2025-10-01 18:08:55 -0300

    add(homelab): open-webui

---
+++ modules/homelab/open-webui/default.nix
@@ -0,0 +1,23 @@
+{ config, lib, ... }:
+let cfg = config.services."open-webui-custom";
+in {
+  options.services."open-webui-custom" = {
+    enable = lib.mkEnableOption "Custom Open-webui configuration";
+    port = lib.mkOption {
+      type = lib.types.port;
+      description = "Open-webui bind address";
+      default = 45555;
+      example = 8080;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.open-webui = {
+      enable = true;
+      openFirewall = true;
+      port = 45555;
+    };
+
+  };
+}
+
