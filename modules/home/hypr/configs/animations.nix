f4d6f688eddd33ad67af0aafed77c752c62d786d
zDyant <dyant@proton.me>
2025-10-27 21:07:30 -0300

    update(hypr): animations

--- modules/home/hypr/configs/animations.nix
+++ modules/home/hypr/configs/animations.nix
@@ -1,23 +1,31 @@
-{ ... }:
-{
-  wayland.windowManager.hyprland =  {
-    extraConfig = '' 
+{...}: {
+  wayland.windowManager.hyprland = {
+    extraConfig = ''
 
- animations {
-  enabled = yes
+       animations {
+        enabled = yes
 
-  bezier = shot, 0.2, 1.0, 0.2, 1.0
-  bezier = swipe, 0.6, 0.0, 0.2, 1.05
-  bezier = linear, 0.0, 0.0, 1.0, 1.0
-  bezier = progressive, 1.0, 0.0, 0.6, 1.0
+        bezier = swipe, 0.6, 0.0, 0.2, 1.05
+        bezier = easeOutQuint,0.23,1,0.32,1
+        bezier = easeInOutCubic,0.65,0.05,0.36,1
+        bezier = linear,0,0,1,1
+        bezier = quick,0.15,0,0.1,1
 
-  
-  animation = windows, 1, 6, shot, slide
-  animation = workspaces, 1, 6, swipe, slide
-  animation = fade, 1, 4, progressive
-  animation = border, 1, 6, linear
-  animation = borderangle, 1, 180, linear, loop #used by rainbow borders and rotating colors
-}   
+        animation = windows,    1, 6, easeOutQuint
+        animation = windowsIn,  1, 6, easeOutQuint, popin 80%
+        animation = windowsOut, 1, 4, linear,       popin
+        animation = windowsMove,1, 4, easeInOutCubic
+
+        animation = fade,       1, 6, easeInOutCubic
+        animation = fadeIn,     1, 6, easeInOutCubic
+        animation = fadeOut,    1, 6, quick
+
+        animation = layers,     1, 6, easeOutQuint
+        animation = layersIn,   1, 6, easeOutQuint, fade
+        animation = layersOut,  1, 4, linear, popin
+
+        animation = workspaces,  1, 6, swipe, slide
+      }
 
     '';
   };
