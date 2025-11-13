47857ee44b402770765ea4c2aa314f67b60b7129
zDyant <dyant@proton.me>
2025-11-13 19:49:56 -0300

    update(homelab): config

--- modules/nixos/homelab.nix
+++ modules/nixos/homelab.nix
@@ -1,5 +1,4 @@
-{ config, ... }:
-{
+{ config, ... }: {
   imports = [ ../homelab/default.nix ];
 
   sops.secrets.cloudflared-token = { };
@@ -11,18 +10,19 @@
 
     services = {
       glance.enable = true;
-      qbittorrent.enable = true;
-      plex.enable = true;
-      prowlarr.enable = true;
-      radarr.enable = true;
-      sonarr.enable = true;
-      flaresolverr.enable = true;
+      # qbittorrent.enable = true;
+      # plex.enable = true;
+      # prowlarr.enable = true;
+      # radarr.enable = true;
+      # sonarr.enable = true;
+      # flaresolverr.enable = true;
       open-webui.enable = true;
       ollama.enable = true;
       kutt = {
         enable = true;
         jwtToken = config.sops.secrets.kutt-token.path;
       };
+      # comet.enable = true;
     };
   };
 }
