43f8ece704a225510093c9db0eaf5a1e5475f8e5
zDyant <dyant@proton.me>
2025-11-30 12:50:55 -0300

    update(nix): gaming

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -1,48 +1,22 @@
-{ pkgs, lib, ... }: 
+{ inputs, pkgs, ... }:
 {
+  imports = [ inputs.play.nixosModules.play ];
 
   hardware.graphics = {
     enable = true;
-    # driSupport = true;
-    # driSupport32Bit = true;
+  };
+
+  play = {
+    amd.enable = true;
+    ananicy.enable = true;
+    gamemode.enable = true;
+
+    steam = {
+      enable = true;
+      extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    };
   };
 
   services.xserver.videoDrivers = [ "amdgpu" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
-
-  # Enabiling this makes gamescope unsuable
-  # https://github.com/ValveSoftware/gamescope/issues/1349
-  # https://github.com/ValveSoftware/gamescope/issues/1465
-  # hardware.graphics.extraPackages = with pkgs; [ amdvlk ];
-  # hardware.graphics.extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-
-  programs.steam = {
-    enable = true;
-    gamescopeSession = {
-      enable = true;
-    };
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-  };
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "steam"
-    "steam-original"
-    "steam-unwrapped"
-    "steam-runtime"
-  ];
-
-  programs.gamemode = {
-   enable = true;
-   enableRenice = true;
-   settings = {
-
-    custom = {
-      start = "notify-send -a 'Gamemode' 'Gamemode ON' ";
-      end = "notify-send -a 'Gamemode' 'Gamemode OFF' ";
-    };
-   };
-  };
 }
-
