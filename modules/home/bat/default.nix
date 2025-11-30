2dbb058b22793c72c4fda24db5b5f938ed3756a3
zDyant <dyant@proton.me>
2025-11-30 10:57:39 -0300

    update(home): bat

--- modules/home/bat/default.nix
+++ modules/home/bat/default.nix
@@ -1,25 +1,6 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.bat = {
     enable = true;
-
-    # config = {
-    #   theme = "1337";
-    # };
-
-    themes = let
-      catppuccin = pkgs.fetchFromGitHub {
-        owner = "catppuccin";
-        repo = "bat";
-        rev = "b19bea35a85a32294ac4732cad5b0dc6495bed32";
-        sha256 = "sha256-POoW2sEM6jiymbb+W/9DKIjDM1Buu1HAmrNP0yC2JPg=";
-      };
-    in {
-      catppuccin-frappe.src     = "${catppuccin}/themes/Catppuccin Frappe.tmTheme";
-      catppuccin-latte.src      = "${catppuccin}/themes/Catppuccin Latte.tmTheme";
-      catppuccin-macchiato.src  = "${catppuccin}/themes/Catppuccin Macchiato.tmTheme";
-      catppuccin-mocha.src      = "${catppuccin}/themes/Catppuccin Mocha.tmTheme";
-
-    };
   };
 }
