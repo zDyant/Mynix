86f30116dc44f39e20c20ad0bc519d1b8e946380
zDyant <dyant@proton.me>
2025-12-07 22:43:38 -0300

    update(home): zsh

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -25,6 +25,7 @@
       nr = "nh os switch -H zdyant";
       ns = "nh search ";
       ls = "exa --icons";
+      lg = "lazygit";
 
 	  da = "devbox add";
 
@@ -53,10 +54,6 @@
   programs.zoxide = {
     enable = true;
     enableZshIntegration = true;
-    # enableBashIntegration = true;
-    # options = [
-    #   "--cmd cd"
-    # ];
   };
 
   programs.fzf = {
