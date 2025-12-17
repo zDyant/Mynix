b2ced92b3a746b7b28cabeeb1a15f4437bd69991
zDyant <dyant@proton.me>
2025-12-17 15:22:06 -0300

    fix(home): wallpaper-engine

--- modules/home/wallpaper-engine/default.nix
+++ modules/home/wallpaper-engine/default.nix
@@ -2,7 +2,6 @@
 {
   services.linux-wallpaperengine = {
     enable = true;
-    clamping = "border";
     assetsPath = "${config.xdg.dataHome}/Steam/steamapps/common/wallpaper_engine/assets";
     wallpapers = [
       {
@@ -16,11 +15,10 @@
 
   # Add env variable for wayland
   systemd.user.services."linux-wallpaperengine" = {
-    environment = {
-      XDG_SESSION_TYPE = "wayland";
-      WAYLAND_DISPLAY = "wayland-1";
-      DISPLAY = ":0";
-    };
-    Service.Environment = "XDG_SESSION_TYPE=wayland DISPLAY=:0";
+    Service.Environment = [
+      "XDG_SESSION_TYPE=wayland"
+      "DISPLAY=:0"
+      "WAYLAND_DISPLAY=wayland-1"
+    ];
   };
 }
