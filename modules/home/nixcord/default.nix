8c2cb945133b9ece22dd8f81916e0e181066142c
zDyant <dyant@proton.me>
2025-12-29 23:09:12 -0300

    update(home): nixcord

--- modules/home/nixcord/default.nix
+++ modules/home/nixcord/default.nix
@@ -1,8 +1,5 @@
 { inputs, ... }: {
-  imports = [
-    inputs.nixcord.homeModules.nixcord
-    ./system24.nix
-  ];
+  imports = [ inputs.nixcord.homeModules.nixcord ./system24.nix ];
   stylix.targets.nixcord.enable = false;
 
   programs.nixcord = {
@@ -18,14 +15,13 @@
       plugins = {
         themeLibrary.enable = true;
         openInApp.enable = true;
-        usrbg.enable = true;
         allCallTimers = {
           enable = true;
           trackSelf = true;
         };
         alwaysAnimate.enable = true;
         alwaysTrust.enable = true;
-        anammox.enable = true;
+        anammox.enable = true; # Removes gift/shop/boost buttons..
         animalese.enable = true;
         bannersEverywhere = {
           enable = true;
@@ -37,42 +33,33 @@
         betterSettings.enable = true;
         biggerStreamPreview.enable = true;
         callTimer.enable = true;
-        clearUrLs.enable = true;
+        ClearURLs.enable = true;
         clipsEnhancements.enable = true;
         colorSighted.enable = true;
         crashHandler.enable = true;
         ctrlEnterSend.enable = true;
         disableDeepLinks.enable = true;
+        equicordToolbox.enable = true;
         equicordHelper = {
           enable = true;
-          # disableCreateDmButton = true;
+          showYourOwnActivityButtons = true;
           noMirroredCamera = true;
         };
-        equicordToolbox.enable = true;
         fakeNitro.enable = true;
         fixImagesQuality.enable = true;
         fixSpotifyEmbeds.enable = true;
         fixYoutubeEmbeds.enable = true;
-        followVoiceUser.enable = true;
         forwardAnywhere.enable = true;
         ghosted.enable = true;
-        gitHubRepos.enable = true;
-        globalBadges.enable = true;
-        googleThat = {
-          enable = true;
-          defaultEngine = "DuckDuckGo";
-        };
         ignoreTerms.enable = true;
         imageFilename.enable = true;
-        jumpscare.enable = true;
         keyboardSounds = {
           enable = true;
-          soundPack = "osu";
+          soundPack = "operagx";
         };
         spotifyShareCommands.enable = true;
         spotifyCrack.enable = true;
         spotifyActivityToggle.enable = true;
-        userPfp.enable = true;
         validReply.enable = true;
         voiceButtons.enable = true;
         voiceChatDoubleClick.enable = true;
@@ -81,8 +68,7 @@
         webRichPresence.enable = true;
         youtubeAdblock.enable = true;
         webScreenShareFixes.enable = true;
-        wigglyText.enable = true;
-        blurNSFW.enable = true;
+        BlurNSFW.enable = true;
       };
     };
   };
