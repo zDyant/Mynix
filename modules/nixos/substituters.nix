fb4851fa57f6bfc12d1a891762182000218d4d1b
zDyant <dyant@proton.me>
2025-12-30 10:48:34 -0300

    update(nix): substituers

--- modules/nixos/substituters.nix
+++ modules/nixos/substituters.nix
@@ -1,14 +1,24 @@
-{ ... }:
-{
+{ ... }: {
   nix.settings = {
     substituters = [
       "https://cache.nixos.org/"
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
-      ];
+      "https://numtide.cachix.org"
+      "https://chaotic-nyx.cachix.org"
+      "https://vicinae.cachix.org"
+      "https://nixpkgs-python.cachix.org"
+      "https://devenv.cachix.org"
+    ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
     ];
   };
 }
