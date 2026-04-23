b247bd96651fcecb8db26e2e99056b6e5eb41850
zDyant <dyant@proton.me>
2026-04-23 11:48:19 -0300

    update(gaia): substituters

--- hosts/gaia/substituters/default.nix
+++ hosts/gaia/substituters/default.nix
@@ -1,5 +1,6 @@
 { ... }: {
   nix.settings = {
+    trusted-users = ["zdyant"];
     substituters = [
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
@@ -8,6 +9,7 @@
       "https://nixpkgs-python.cachix.org"
       "https://devenv.cachix.org"
       "https://cache.garnix.io"
+      "https://cache.nixos.org?priority=1"
     ];
     trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
@@ -17,6 +19,7 @@
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
 }
