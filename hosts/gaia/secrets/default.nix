739457a59ac3a743fa5fe013905e795f8ae8ea87
zDyant <dyant@proton.me>
2026-05-01 13:10:04 -0300

    update(gaia): remove redudant secrets.nix code

--- hosts/gaia/secrets/default.nix
+++ hosts/gaia/secrets/default.nix
@@ -1,15 +1,8 @@
-{ pkgs, inputs, config, ...}:
+{ lib, flakeRoot, ...}:
 {
-  imports = [
-    inputs.sops-nix.nixosModules.sops
-  ];
-
-  sops.defaultSopsFile = ../../../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-
-  sops.age.keyFile = "/home/zdyant/.config/sops/age/keys.txt";
-
-  sops.secrets.gemini_api_key = {
-    owner = config.users.users.zdyant.name;
+  sops = {
+    defaultSopsFile = lib.fs.relativeTo flakeRoot "secrets/secrets.yaml";
+    defaultSopsFormat = "yaml";
+    age.keyFile = "/home/zdyant/.config/sops/age/keys.txt";
   };
 }
