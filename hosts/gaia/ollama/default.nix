a26d638d83d2f876380475fcd3c1aa0730410531
zDyant <dyant@proton.me>
2026-05-01 12:43:17 -0300

    add(gaia): ollama

---
+++ hosts/gaia/ollama/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  services.ollama = {
+    enable = true;
+    openFirewall = true;
+    package = pkgs.ollama-vulkan;
+    environmentVariables = {
+      # Ryzen 5 5600 / 9060xt 16gb
+      OLLAMA_NUM_THREADS="12";
+      OLLAMA_FLASH_ATTENTION="1";
+      OLLAMA_MAX_LOADED_MODELS = "1";
+    };
+    loadModels = [
+      "qwen3.5:9b"
+      "sweepai/sweep-next-edit:latest"
+    ];
+  };
+}
