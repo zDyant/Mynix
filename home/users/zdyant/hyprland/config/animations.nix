c254f5c2412c621b9617cfa0e32af3524ff4f764
zDyant <dyant@proton.me>
2026-05-01 15:13:55 -0300

    update(zdyant): hyprland animations

--- home/users/zdyant/hyprland/config/animations.nix
+++ home/users/zdyant/hyprland/config/animations.nix
@@ -1,28 +1,25 @@
-{...}: {
+{lib, ...}: {
   wayland.windowManager.hyprland.settings.animations = {
     enabled = true;
     bezier = [
       "emphasizedAccel, 0.3, 0, 0.8, 0.15"
       "emphasizedDecel, 0.05, 0.7, 0.1, 1"
       "standard, 0.2, 0, 0, 1"
-      "swipe, 0.6, 0.0, 0.2, 1.05"
+      "swipe, 0.4, 0, 0.2, 1"
     ];
 
-    animation = [
-      "layersIn,         1, 5, emphasizedDecel, slide"
-      "layersOut,        1, 4, emphasizedAccel, slide"
-      "fadeLayers,       1, 4, standard"
+    animation = lib.mkForce [
+      "layersIn,         1, 5, emphasizedDecel, popin"
+      "layersOut,        1, 4, emphasizedAccel, popin"
 
-      "windowsIn,        1, 5, emphasizedDecel, gnomed"
-      "windowsOut,       1, 5, emphasizedAccel, gnomed"
+      "windowsIn,        1, 5, emphasizedDecel, slide"
+      "windowsOut,       1, 4, emphasizedAccel, gnomed"
       "windowsMove,      1, 5, standard"
 
-      "workspaces,       1, 4, swipe, slidefade 20%"
-      "specialWorkspace, 1, 4, swipe, slidefadevert 20%"
+      "workspaces,       1, 5, swipe, slidevert"
 
-      "fade,             1, 6, emphasizedDecel"
-      "fadeDim,          1, 6, standard"
-      "border,           1, 6, standard"
+      "fade,             1, 5, swipe"
+      "fadeOut,          1, 4, emphasizedAccel"
     ];
   };
 }
