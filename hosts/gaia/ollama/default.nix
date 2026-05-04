f87c6af84814a2b85881d37afa1d2a3062cc9f84
zDyant <dyant@proton.me>
2026-05-04 18:18:31 -0300

    update ollama: increase ollama_max_loaded_model

--- hosts/gaia/ollama/default.nix
+++ hosts/gaia/ollama/default.nix
@@ -8,7 +8,7 @@
       # Ryzen 5 5600 / 9060xt 16gb
       OLLAMA_NUM_THREADS="12";
       OLLAMA_FLASH_ATTENTION="1";
-      OLLAMA_MAX_LOADED_MODELS = "1";
+      OLLAMA_MAX_LOADED_MODELS = "2";
     };
     loadModels = [
       "qwen3.5:9b"
