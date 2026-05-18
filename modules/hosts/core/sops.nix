9dcb821538cf982c17afe20774d2094df1bdc165
zDyant <dyant@proton.me>
2026-05-17 22:07:00 -0300

    update(hosts/core): move and update sops.nix

--- modules/hosts/core/sops.nix
+++ modules/hosts/core/sops.nix
@@ -3,6 +3,6 @@
   sops = {
     defaultSopsFile = lib.fs.relativeTo flakeRoot "secrets/secrets.yaml";
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/zdyant/.config/sops/age/keys.txt";
+    age.keyFile = "/sops.txt";
   };
 }
