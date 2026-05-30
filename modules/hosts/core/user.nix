8f177c7996a926d69e310544d708de0cc6cce35a
zDyant <dyant@proton.me>
2026-05-30 20:02:46 -0300

    update hosts/core: user.nix

--- modules/hosts/core/user.nix
+++ modules/hosts/core/user.nix
@@ -1,7 +1,28 @@
-{host, ...}: let
+{
+  host,
+  pkgs,
+  lib,
+  secrets,
+  ...
+}: let
   user = host.user;
+  userSecrets = secrets.users.${user.name} or {};
 in {
+  users.users.${user.name} = {
+    createHome = true;
+    description = "Admin";
+    homeMode = "750";
+    hashedPassword = userSecrets.hashedPassword or null;
+    openssh.authorizedKeys.keys = userSecrets.ssh.publicKeys or [];
+  };
+
   # Special sudo config for user
+  users.users.root = {
+    shell = pkgs.zsh;
+    hashedPassword = lib.mkForce (userSecrets.hashedPassword or null);
+    openssh.authorizedKeys.keys = userSecrets.ssh.publicKeys or [];
+  };
+
   security.sudo = {
     extraConfig = ''
       Defaults lecture = never
@@ -9,6 +30,7 @@ in {
       Defaults timestamp_timeout=120
       Defaults env_keep+=SSH_AUTH_SOCK
     '';
+
     extraRules = [
       {
         users = [user.name];
