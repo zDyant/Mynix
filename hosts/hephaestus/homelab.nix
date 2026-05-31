ab002e13043b24b67549ca3f167b7ac3377e7ef6
zDyant <dyant@proton.me>
2026-05-31 19:42:04 -0300

    add hephaestus: homelab-config

---
+++ hosts/hephaestus/homelab.nix
@@ -0,0 +1,19 @@
+{
+  lib,
+  flakeRoot,
+  ...
+}: {
+  imports = (map (lib.fs.relativeTo flakeRoot) [
+    "modules/homelab"
+  ]);
+
+  homelab = {
+    enable = true;
+    domain = "zdyant.com";
+
+    services = {
+      glance.enable = true;
+      openmonetis.enable = true;
+    };
+  };
+}
