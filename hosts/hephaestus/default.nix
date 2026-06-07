87a2064279d24f76c24f2936c7445c3062b7f142
zDyant <dyant@proton.me>
2026-06-07 18:26:58 -0300

    feat: remove core used config

--- hosts/hephaestus/default.nix
+++ hosts/hephaestus/default.nix
@@ -3,7 +3,6 @@
   time.timeZone = "UTC";
 
   # environment.systemPackages = with pkgs; [ ];
-  programs.nix-ld.enable = true;
 
   # Only for root's password reset on hetzner
   services.qemuGuest.enable = true;
