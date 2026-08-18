f4a4a1891382c90e2cbee5c35cebc7632c1956f9
zDyant <dyant@proton.me>
2026-08-17 23:17:22 -0300

    fix(nix): remove unavailable garnix cache

--- modules/hosts/common/cache.nix
+++ modules/hosts/common/cache.nix
@@ -7,7 +7,6 @@
       "https://vicinae.cachix.org"
       "https://nixpkgs-python.cachix.org"
       "https://devenv.cachix.org"
-      "https://cache.garnix.io"
       "https://cache.nixos.org?priority=1"
     ];
     trusted-public-keys = [
@@ -17,7 +16,6 @@
       "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
   };
