cc706dab6ccf3933bfd448094232f854b9b1cc20
zDyant <dyant@proton.me>
2026-08-21 19:44:02 -0300

    feat(spicetify): update theme

--- modules/home/common/spicetify.nix
+++ modules/home/common/spicetify.nix
@@ -1,3 +1,4 @@
+# https://github.com/adam01110/infra/blob/6e2c76c12162068a05b0b725816b47738f6b2a1c/modules/programs/gui/spotify/theme.nix#
 {
   lib,
   config,
@@ -5,29 +6,95 @@
   inputs,
   ...
 }: let
-  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  inherit (pkgs.stdenv.hostPlatform) system;
+  inherit (config.lib.stylix) colors;
+
+  font = config.stylix.fonts.monospace.name;
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${system};
 in {
   imports = [inputs.spicetify-nix.homeManagerModules.default];
   stylix.targets.spicetify.enable = false;
 
   programs.spicetify = lib.mkDefault {
     enable = true;
-    theme = spicePkgs.themes.text;
+    theme = {
+      name = "stylix";
+      inherit (spicePkgs.themes.text) src;
+      additionalCss =
+        #css
+        ''
+          :root {
+          --font-family: '${font}', monospace !important;
+          --display-card-image: block !important;
+          --display-coverart-image: block !important;
+          --display-header-image: block !important;
+          --display-sidebar-image: block !important;
+          --display-tracklist-image: block !important;
+          --border-transition: 0s !important;
+          }
+                    .Root__globalNav,
+          .main-yourLibraryX-entryPoints,
+          .Root__main-view,
+          .main-nowPlayingBar-container,
+          .Root__right-sidebar:has(aside:not(:empty)) {
+            transition: none !important;
+          }
+
+          .Root__globalNav::before,
+          .Root__nav-bar .main-yourLibraryX-entryPoints::before,
+          .Root__main-view::before,
+          .main-nowPlayingBar-container::before,
+          .Root__right-sidebar:has(aside:not(:empty))::before {
+            transition: none !important;
+          }
+
+          .player-controls__buttons,
+          .main-nowPlayingBar-extraControls,
+          .main-connectBar-connectBar {
+            opacity: 1 !important;
+            transition: none !important;
+          }
+
+          *,
+          *::before,
+          *::after {
+            border-radius: 0px !important;
+          }
+
+          .view-homeShortcutsGrid-shortcuts::before {
+            content: "" !important;
+          }
+        '';
+    };
     colorScheme = "custom";
-    customColorScheme = with config.lib.stylix.colors; {
-      "accent" = "${base0C}";
-      "accent-active" = "${base0D}";
-      "accent-inactive" = "${base01}";
-      "banner" = "${base0A}";
-      "border-active" = "${base0A}";
-      "border-inactive" = "${base02}";
-      "header" = "${base03}";
-      "highlight" = "${base07}";
-      "main" = "${base00}";
-      "notification" = "${base0B}";
-      "notification-error" = "${base08}";
-      "subtext" = "${base04}";
-      "text" = "${base05}";
+    customColorScheme = with colors; {
+      accent = base0C;
+      accent-active = base0D;
+      accent-inactive = base01;
+      text = base05;
+
+      banner = base0C;
+      border-active = base0A;
+      border-inactive = base02;
+      header = base03;
+      highlight = base02;
+      main = base00;
+      notification = base0C;
+      notification-error = base08;
+      subtext = base04;
+      button = base04;
+      button-active = base04;
+      button-disabled = base03;
+      card = base03;
+      equalizer = base0C;
+      highlight-elevated = base03;
+      main-elevated = base02;
+      misc = base02;
+      player = base04;
+      selected-row = base04;
+      shadow = base00;
+      sidebar = base01;
+      tab-active = base02;
     };
 
     # enabledCustomApps = with spicePkgs.apps; [ ];
