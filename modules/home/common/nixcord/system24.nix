b7b1dece810502660a3b8fc73b13c7853e5e786a
zDyant <dyant@proton.me>
2026-09-05 21:00:16 -0300

    feat(nixcord): update system24 theme

--- modules/home/common/nixcord/system24.nix
+++ modules/home/common/nixcord/system24.nix
@@ -1,160 +1,139 @@
-{config, ...}: {
-  programs.nixcord.quickCss = ''
-        /*
+{config, ...}: let
+  colors = config.lib.stylix.colors.withHashtag;
+in {
+  programs.nixcord.quickCss =
+    #css
+    ''
+      /**
          * @name system24
          * @description a tui-style discord theme.
          * @author refact0r
-         * @version 2.0.0
+         * @version 2.1.0
          * @invite nz87hXyvcy
          * @website https://github.com/refact0r/system24
          * @source https://github.com/refact0r/system24/blob/master/theme/system24.theme.css
          * @authorId 508863359777505290
          * @authorLink https://www.refact0r.dev
-        */
+      */
 
-        /* import theme modules */
-        @import url('https://refact0r.github.io/system24/build/system24.css');
+      /* import theme modules */
+      @import url('https://refact0r.github.io/system24/build/system24.css');
 
-        body {
-            --font: 'DM Mono'; /* change to ' ' for default discord font */
-            --code-font: 'DM Mono'; /* change to ' ' for default discord font */
-            font-weight: 300; /* text font weight. 300 is light, 400 is normal. DOES NOT AFFECT BOLD TEXT */
-            letter-spacing: -0.05ch; /* decreases letter spacing for better readability. recommended on monospace fonts.*/
+      body {
+          /* font, change to "" for default discord font */
+          --font: 'DM MONO';
+          --code-font: 'DM MONO';
+          font-weight: 300;
+          letter-spacing: -0.05ch;
 
-            /* sizes */
-            --gap: 12px; /* spacing between panels */
-            --divider-thickness: 4px; /* thickness of unread messages divider and highlighted message borders */
-            --border-thickness: 1px; /* thickness of borders around main panels. DOES NOT AFFECT OTHER BORDERS */
-            --border-hover-transition: 0.2s ease; /* transition for borders when hovered */
+          /* sizes */
+          --gap: 12px;
+          --divider-thickness: 4px;
+          --border-thickness: 1px;
+          --border-hover-transition: 0.2s ease;
 
-            /* animation/transition options */
-            --animations: on; /* off: disable animations/transitions, on: enable animations/transitions */
-            --list-item-transition: 0.4s ease; /* transition for list items */
-            --dms-icon-svg-transition: 0.6s ease; /* transition for the dms icon */
+          /* animation/transition options */
+          --animations: on;
+          --list-item-transition: 0.2s ease;
+          --dms-icon-svg-transition: 0.4s ease;
 
-            /* top bar options */
-            --top-bar-height: var(--gap); /* height of the top bar (discord default is 36px, old discord style is 24px, var(--gap) recommended if button position is set to titlebar) */
-            --top-bar-button-position: titlebar; /* off: default position, hide: hide buttons completely, serverlist: move inbox button to server list, titlebar: move inbox button to channel titlebar (will hide title) */
-            --top-bar-title-position: off; /* off: default centered position, hide: hide title completely, left: left align title (like old discord) */
-            --subtle-top-bar-title: off; /* off: default, on: hide the icon and use subtle text color (like old discord) */
+          /* top bar options */
+          --top-bar-height: var(--gap);
+          --top-bar-button-position: titlebar;
+          --top-bar-title-position: off;
+          --subtle-top-bar-title: off;
 
-            /* window controls */
-            --custom-window-controls: on; /* off: default window controls, on: custom window controls */
-            --window-control-size: 14px; /* size of custom window controls */
+          /* window controls */
+          --custom-window-controls: off;
+          --window-control-size: 14px;
 
-            /* dms button options */
-            --custom-dms-icon: off; /* off: use default discord icon, hide: remove icon entirely, custom: use custom icon */
-            --dms-icon-svg-url: url(""); /* icon svg url. MUST BE A SVG. */
-            --dms-icon-svg-size: 90%; /* size of the svg (css mask-size property) */
-            --dms-icon-color-before: var(--icon-secondary); /* normal icon color */
-            --dms-icon-color-after: var(--white); /* icon color when button is hovered/selected */
-            --custom-dms-background: off; /* off to disable, image to use a background image (must set url variable below), color to use a custom color/gradient */
-            --dms-background-image-url: url(""); /* url of the background image */
-            --dms-background-image-size: cover; /* size of the background image (css background-size property) */
-            --dms-background-color: linear-gradient(70deg, var(--blue), var(--purple), var(--red)); /* fixed color/gradient (css background property) */
+          /* dms button options */
+          --custom-dms-icon: custom;
+          --dms-icon-svg-url: url("");
+          --dms-icon-svg-size: 90%;
+          --dms-icon-color-before: var(--icon-subtle);
+          --dms-icon-color-after: var(--white);
+          --custom-dms-background: off;
+          --dms-background-image-url: url("");
+          --dms-background-image-size: cover;
+          --dms-background-color: linear-gradient(70deg, var(--blue-2), var(--purple-2), var(--red-2));
 
-            /* background image options */
-            --background-image: off; /* off: no background image, on: enable background image (must set url variable below) */
-            --background-image-url: url(""); /* url of the background image */
+          /* background image options */
+          --background-image: off;
+          --background-image-url: url("");
 
-            /* transparency/blur options */
-            /* NOTE: TO USE TRANSPARENCY/BLUR, YOU MUST HAVE TRANSPARENT BG COLORS. FOR EXAMPLE: --bg-4: hsla(220, 15%, 10%, 0.7); */
-            --transparency-tweaks: off; /* off: no changes, on: remove some elements for better transparency */
-            --remove-bg-layer: off; /* off: no changes, on: remove the base --bg-3 layer for use with window transparency (WILL OVERRIDE BACKGROUND IMAGE) */
-            --panel-blur: off; /* off: no changes, on: blur the background of panels */
-            --blur-amount: 12px; /* amount of blur */
-            --bg-floating: var(--bg-3); /* set this to a more opaque color if floating panels look too transparent. only applies if panel blur is on  */
+          /* transparency/blur options */
+          --transparency-tweaks: off;
+          --remove-bg-layer: off;
+          --panel-blur: off;
+          --blur-amount: 12px;
+          --bg-floating: var(--bg-3);
 
-            /* other options */
-            --small-user-panel: on; /* off: default user panel, on: smaller user panel like in old discord */
+          /* other options */
+          --small-user-panel: on;
 
-            /* unrounding options */
-            --unrounding: on; /* off: default, on: remove rounded corners from panels */
+          /* unrounding options */
+          --unrounding: on;
+          --round-pfp: off;
+          --remove-pfp-decor: off;
 
-            /* styling options */
-            --custom-spotify-bar: on; /* off: default, on: custom text-like spotify progress bar */
-            --ascii-titles: on; /* off: default, on: use ascii font for titles at the start of a channel */
-            --ascii-loader: system24; /* off: default, system24: use system24 ascii loader, cats: use cats loader */
+          /* styling options */
+          --custom-spotify-bar: on;
+          --ascii-titles: on;
+          --ascii-loader: system24;
 
-            /* panel labels */
-            --panel-labels: on; /* off: default, on: add labels to panels */
-            --label-color: var(--text-muted); /* color of labels */
-            --label-font-weight: 500; /* font weight of labels */
-        }
+          /* panel labels */
+          --panel-labels: on;
+          --label-color: var(--text-muted);
+          --label-font-weight: 500;
+      }
 
+      /* color options */
       :root {
-        --colors: on;
+          --colors: on;
 
-        /* text colors */
-        --text-0: ${config.lib.stylix.colors.withHashtag.base01};
-        --text-1: ${config.lib.stylix.colors.withHashtag.base05};
-        --text-2: ${config.lib.stylix.colors.withHashtag.base06};
-        --text-3: ${config.lib.stylix.colors.withHashtag.base04};
-        --text-4: ${config.lib.stylix.colors.withHashtag.base03};
-        --text-5: ${config.lib.stylix.colors.withHashtag.base07};
+          /* text colors */
+          --text-0: ${colors.base01}; /* text on colored elements */
+          --text-1: ${colors.base05}; /* other normally white text */
+          --text-2: ${colors.base06}; /* headings and important text */
+          --text-3: ${colors.base04}; /* normal text */
+          --text-4: ${colors.base03}; /* icon buttons and channels */
+          --text-5: ${colors.base07}; /* muted channels/chats and timestamps */
 
-        /* background colors (dark → light) */
-        --bg-4: ${config.lib.stylix.colors.withHashtag.base00};
-        --bg-3: ${config.lib.stylix.colors.withHashtag.base01};
-        --bg-2: ${config.lib.stylix.colors.withHashtag.base02};
-        --bg-1: ${config.lib.stylix.colors.withHashtag.base03};
+          /* background and dark colors */
+          --bg-1: ${colors.base03};
+          --bg-2: ${colors.base02};
+          --bg-3: ${colors.base01};
+          --bg-4: ${colors.base00};
+          --hover: color-mix(in srgb, ${colors.base05} 10%, transparent);
+          --active: color-mix(in srgb, ${colors.base05} 20%, transparent);
+          --active-2: color-mix(in srgb, ${colors.base05} 30%, transparent);
+          --message-hover: var(--hover);
 
-        /* hover + active */
-        --hover: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base05} 10%, transparent);
-        --active: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base05} 20%, transparent);
-        --active-2: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base05} 30%, transparent);
-        --message-hover: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base05} 10%, transparent);
+          /* accent colors */
+          --accent-1: ${colors.base0A}; /* links and other accent text */
+          --accent-2: ${colors.base0D}; /* small accent elements */
+          --accent-3: ${colors.base0D}; /* accent buttons */
+          --accent-4: color-mix(in srgb, ${colors.base0D} 80%, white); /* accent buttons hover */
+          --accent-5: color-mix(in srgb, ${colors.base0D} 70%, white); /* accent buttons clicked */
+          --accent-new: ${colors.base08}; /* mute/deafen etc */
+          --mention: linear-gradient(to right, color-mix(in oklch, var(--accent-2), transparent 90%) 40%, transparent);
+          --mention-hover: linear-gradient(to right, color-mix(in oklch, var(--accent-2), transparent 95%) 40%, transparent);
+          --reply: linear-gradient(to right, color-mix(in oklch, var(--text-3), transparent 90%) 40%, transparent);
+          --reply-hover: linear-gradient(to right, color-mix(in oklch, var(--text-3), transparent 95%) 40%, transparent);
 
-        /* accent colors */
-        --accent-1: ${config.lib.stylix.colors.withHashtag.base0A}; /* links */
-        --accent-2: ${config.lib.stylix.colors.withHashtag.base0D}; /* UI accents */
-        --accent-3: ${config.lib.stylix.colors.withHashtag.base0D};
-        --accent-4: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base0D} 80%, white);
-        --accent-5: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base0D} 70%, white);
+          /* status indicator colors */
+          --online   : ${colors.base0C};
+          --dnd      : ${colors.base08};
+          --idle     : ${colors.base0E};
+          --streaming: ${colors.base0D};
+          --offline  : var(--text-4);
 
-        --accent-new: ${config.lib.stylix.colors.withHashtag.base08}; /* red-like */
-
-        --mention: linear-gradient(
-            to right,
-            color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base0D} 10%, transparent) 40%,
-            transparent
-        );
-        --mention-hover: linear-gradient(
-            to right,
-            color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base0D} 5%, transparent) 40%,
-            transparent
-        );
-
-        --reply: linear-gradient(
-            to right,
-            color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base04} 10%, transparent) 40%,
-            transparent
-        );
-        --reply-hover: linear-gradient(
-            to right,
-            color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base04} 5%, transparent) 40%,
-            transparent
-        );
-
-        /* status indicator colors */
-        --online   : var(--green );
-        --dnd      : var(--red);
-        --idle     : var(--yellow);
-        --streaming: var(--purple);
-        --offline  : var(--text-4);
-
-        /* borders */
-        --border-light: var(--hover);
-        --border: var(--active);
-        --border-hover: var(--accent-1);
-        --button-border: color-mix(in srgb, ${config.lib.stylix.colors.withHashtag.base06} 10%, transparent);
-
-        /* base semantic colors */
-        --red:    ${config.lib.stylix.colors.withHashtag.base08};
-        --green:  ${config.lib.stylix.colors.withHashtag.base0C};
-        --blue:   ${config.lib.stylix.colors.withHashtag.base0B};
-        --yellow: ${config.lib.stylix.colors.withHashtag.base0E};
-        --purple: ${config.lib.stylix.colors.withHashtag.base0D};
-    }
-  '';
+          /* border colors */
+          --border-light: var(--hover);
+          --border: var(--active);
+          --border-hover: var(--accent-1);
+          --button-border: color-mix(in srgb, ${colors.base06} 10%, transparent);
+      }
+    '';
 }
